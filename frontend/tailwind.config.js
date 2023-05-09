e71886604c0250da4fe28f282bd0bb6a07f55f6e
MazaArt <84805964+MazaArt@users.noreply.github.com>
2023-05-09 15:12:38 -0400

    Tailwind CSS initialized

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/App.js", "./src/index.js", "./src/setupTests.js", "./src/reportWebVitals.js", "./src/App.test.js"  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
